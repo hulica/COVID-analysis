a8903f0f4124e086e3407d9c827e1ddfa79559ea
hulica <linczer.andrea@gmail.com>
2021-06-13 21:31:12 +0200

    Test commit

--- covid_data_analysis_pro_ppl.py
+++ covid_data_analysis_pro_ppl.py
@@ -3,6 +3,7 @@
 # https://github.com/CSSEGISandData/COVID-19/blob/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
 # Thanks to them for making this data set public.
 
+
 import csv
 import matplotlib.pyplot as plt
 import wget
